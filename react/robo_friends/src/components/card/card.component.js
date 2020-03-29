b5baf732490504ea4494b7656a68637f26031bd2
Oddisey000 <oddisey000@gmail.com>
2020-03-29 15:10:08 +0300

    Setting up icons

--- react/robo_friends/src/components/card/card.component.js
+++ react/robo_friends/src/components/card/card.component.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({id, name, email}) => {
   return (
     <div className="bg-light-green dib br3 ma2 grow bw2 shadow-5 tc">
-      <img src={`https://robohash.org/${id}?200x200`} alt="The robot for the card"/>
+      <img src={`https://robohash.org/${id}?`} alt="The robot for the card"/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- react/robo_friends/src/components/search-box/search-box.component.js
+++ react/robo_friends/src/components/search-box/search-box.component.js
@@ -8,6 +8,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Search Robots"
         className="pa3 ba b--green bg-lightest-blue"
         type="search" 
         placeholder="search robots"
